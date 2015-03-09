4b39eaf8dc89bf94397f371bc940d3a4504ac1b4
am2990 <apurv.mehra@gmail.com>
2015-03-10 01:17:03 +0530

    Code Cleaning

--- Framework/src/main/java/com/chs/dao/DissagregationDAOImpl.java
+++ Framework/src/main/java/com/chs/dao/DissagregationDAOImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.chs.entity.ConceptDictionary;
 import com.chs.entity.DissagregationDictionary;
 
 @Transactional 
